aa5a31782dd8810ec30e6ca386f4ffc19836dd7f
zyrouge <naveencnaveen96@gmail.com>
2022-02-11 14:05:55 +0530

    chore(tests): use mpeg

--- test/m3u8_test.dart
+++ test/m3u8_test.dart
@@ -8,7 +8,11 @@ Future<void> main() async {
     'https://bitdash-a.akamaihd.net/content/MI201109210084_1/m3u8s/f08e80da-bf1d-4e3d-8899-f0f6155f6efa_video_180_250000.m3u8',
   );
 
-  const downloader = Downloader(provider: M3U8DLProvider());
+  const downloader = Downloader(
+    provider:
+        M3U8DLProvider(outputFileExtension: M3U8OutputFileExtensions.mpeg),
+  );
+
   final trashDir = await getTrashDir();
 
   group(
